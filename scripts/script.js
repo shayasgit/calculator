211645828f637b3033f6ee06afebcf185dac7bc9
Shayas R.A <shayas.contact@gmail.com>
2024-05-15 23:50:17 +0530

    Add clear button event to clear the display

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
 console.log('Calculator')
+
+let display = document.querySelector('.display');
+display.textContent = '';
+let buttons = document.querySelectorAll('button');
+
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (button.value == 'all-clear') {
+            display.textContent = ''
+        }
+    })
+})
