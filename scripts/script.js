edc4038b7478ee89ad0c178ca11c09ae219f75a2
Shayas R.A <shayas.contact@gmail.com>
2024-05-16 00:44:15 +0530

    Added Function to get Input Numbers to display

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,47 @@
-console.log('Calculator')
+console.log("Calculator");
 
-let display = document.querySelector('.display');
-display.textContent = '';
-let buttons = document.querySelectorAll('button');
+let display = document.querySelector(".display");
+display.textContent = "0";
+let buttons = document.querySelectorAll("button");
+let hasDecimal = false;
 
+function getInput(button) {
+  if (button.value == "all-clear") {
+    display.textContent = "0";
+    hasDecimal = false;
+  }
+  if (display.textContent.length == 12) {
+    return;
+  }
+  if (display.textContent == "0") {
+    if (button.value == ".") {
+      display.textContent = "0.";
+      hasDecimal = true;
+    }
+  }
+  if (button.classList == "number") {
+    if (display.textContent.includes(".")) {
+      hasDecimal = true;
+    }
+    if (hasDecimal) {
+      if (button.value == ".") {
+        display.textContent = display.textContent;
+      } else {
+        display.textContent += button.value;
+      }
+    } else {
+      if (display.textContent[0] == "0") {
+        display.textContent = "";
+        display.textContent += button.value;
+      } else {
+        display.textContent += button.value;
+      }
+    }
+  }
+}
 
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        if (button.value == 'all-clear') {
-            display.textContent = ''
-        }
-    })
-})
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    getInput(button);
+  });
+});
